e645e185115cdff9d246b748e4323a46ebe49161
mrsmith <mamhigtt@gmail.com>
2024-10-13 14:38:00 +0700

    Add basket item babble

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,56 +1,103 @@
 import { useState } from "react";
 
 interface HeaderProps {
-    style?: any, 
-    init: string, 
-    categories: string[], 
-    onChange: (value: string)=>void,
+    style?: any,
+    init: string,
+    categories: string[],
+    onChange: (value: string) => void,
 }
 
-export const Header = ({style, init, categories, onChange = ()=>{}}: HeaderProps) => {
+interface ButtonProps {
+    isActive?: boolean,
+    itemsInBasket?: number,
+    onClick?: ()=>void,
+    children?: any,
+}
+
+const Button = ({isActive, children, onClick, itemsInBasket}: ButtonProps) => {
+    const handleClick = () => {
+        if(onClick !== undefined) {
+            onClick()
+        }
+    }
+
+    return (
+        <div style={{
+                paddingTop: "14px", 
+                position: "relative",
+            }}
+        >
+            <div style={{
+                    position: "absolute", 
+                    top: "0px", 
+                    right: "0px", 
+                    marginRight: "4px",
+                    
+                    width: "16px", 
+                    height: "16px", 
+                    backgroundColor: "green",
+                    borderRadius: "4px",
+
+                    textAlign: "center",
+                    color: "white",
+                }}
+            >{itemsInBasket}</div>
+            <div
+            style={{
+                color: isActive ? "#17181C" : "rgba(255, 255, 255, 0.4)",
+
+                paddingLeft: "6px",
+                paddingRight: "6px",
+                paddingTop: "4px",
+                paddingBottom: "4px",
+
+                borderRadius: "6px",
+
+                backgroundColor: isActive ? "#AAADB2" : undefined,
+                outline: isActive ? undefined : "1px solid rgba(255, 255, 255, 0.4)"
+            }} 
+            onClick={handleClick}
+            >
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export const Header = ({ style, init, categories, onChange = () => { } }: HeaderProps) => {
     const [selected, setSelected] = useState<number>(0)
 
     return (
         <div style={{
-            backgroundColor: "#17181C", 
+            backgroundColor: "#17181C",
 
             borderBottomStyle: "solid",
-            borderBottomWidth: "1px", 
-            borderBottomColor: "#5f6368", 
-            
+            borderBottomWidth: "1px",
+            borderBottomColor: "#5f6368",
+
             display: "flex",
             flexDirection: "row",
             overflowY: "hidden",
             alignItems: "center",
             justifyContent: "center",
-            
+
             padding: "12px",
             gap: "12px",
             ...style
         }}>
-            {categories.map((item, index)=>{
-                const isActive = index === selected
+            {categories.map((item, index) => {
                 return (
-                    <div 
-                        key={index} 
-                        style={{
-                            color: isActive ? "#17181C" : "rgba(255, 255, 255, 0.4)", 
-                            
-                            paddingLeft: "6px", 
-                            paddingRight: "6px", 
-                            paddingTop: "4px", 
-                            paddingBottom: "4px",
-
-                            borderRadius: "6px",
-            
-                            backgroundColor: isActive ? "#AAADB2" : undefined,
-                            outline: isActive ? undefined : "1px solid rgba(255, 255, 255, 0.4)"
-                        }} 
-                        onClick={()=>{
+                    <Button 
+                        key={index}
+                        isActive = {index === selected}
+                        itemsInBasket = {3}
+                        onClick={() => {
                             onChange(categories[index])
                             setSelected(index)
-                        }}>{item}
-                    </div>
+                        }}
+                    >
+                        {item}
+                    </Button>
                 )
             })}
         </div>
